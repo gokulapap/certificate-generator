1ac3483151b27c069019f38349e02ce5a4aaf167
GOKUL A.P <57899332+gokulapap@users.noreply.github.com>
2021-08-28 14:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello():
 	<u><h1>Certificate Generator</h1></u>
 	<br><br>
 
-	<h3>Go to this URL to generate your certificate <br><br> https://certificate-creator.herokuapp.com/certificate/{name}</h3>
+	<h3>Add this to the URL to generate your certificate <br><br> /certificate/{name}</h3>
 
 	</center>
 	'''
